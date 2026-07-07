a616200aadae5f50cc3db41f0bd36be557f2417a
Masa104nanoyo <info@raiselab.net>
2026-07-07 18:48:47 +0900

    Create api-config.js

---
+++ js/api-config.js
@@ -0,0 +1,5 @@
+/**
+ * GAS Web AppのデプロイURLをここに設定する。
+ * デプロイし直してURLが変わった場合はこの1ファイルだけ更新すればよい。
+ */
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxUoC8v5fznTnqITEw-4-miRHQvGXfduTmIcn31kX6G67mrgEvMLrnWKryF1Dl-w7uWOA/exec';
